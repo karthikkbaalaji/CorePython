df830184bca9f7d82572103b1f0f1127a658c09b
Karthik Balaji Mahadevarao Premnath <kpremnath@salesforce.com>
2016-10-26 22:24:48 -0700

    Updated Lists

--- lists.py
+++ lists.py
@@ -1,3 +1,11 @@
-list1 = ['1', '3','5']
-list1.append('8')
-print (list1)
+#write a Python program to maintain two lists and loop
+#until the user wants to quit
+#your program should offer the user the following options:
+#   add an item to list 1 or 2
+#   remove an item from list 1 or 2 by value or index
+#   reverse list 1 or list 2
+#   display both lists
+#EXTRA: add an option to check if lists are equal, even if
+#contents are not in the same order (i.e, if list 1 is [3, 2,
+#'apple', 4] and list 2 is [2, 3, 4, 'apple'], you
+#should indicate they are the same)

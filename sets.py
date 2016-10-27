607657480896207c333a5bda57d63847067fa1d0
Karthik Balaji Mahadevarao Premnath <kpremnath@salesforce.com>
2016-10-27 15:53:03 -0700

    Added sets

---
+++ sets.py
@@ -0,0 +1,19 @@
+# Use a set to find all of the unique words in the input and print them out in sorted order.
+# If the user entered "There is no there there", your program should print out
+#   is
+#   no
+#   there
+# Note that "There" and "there" should be counted as the same word.
+
+from __future__ import print_function
+
+inputString = raw_input("Enter a string: ")
+
+list = inputString.split()
+set = set(list)
+sortedList = sorted(set)
+
+for item in sortedList:
+    print(item,end=' ')
+
+#print(sortedList)

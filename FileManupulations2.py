fa408cc443915df77734ad80c2de4d60edaf9504
Karthik Balaji Mahadevarao Premnath <kpremnath@salesforce.com>
2016-10-28 09:50:21 -0700

    Advanced file manulation

--- FileManupulations2.py
+++ FileManupulations2.py
@@ -4,7 +4,9 @@
 # your program should treat 'The' and 'the' as the same word for counting purposes
 # print out the words and their counts, from least common to most common
 
-import string
+
+#Get punctuation characters from string module
+from string import punctuation
 
 #filename = input('Enter a filename: ')
 filename = '/tmp/poem.txt'
@@ -13,9 +15,22 @@ count = {}
 
 for line in open(filename):
     line = line.lower()
+
+    '''
+    Use a listcomp to "explode" the line into a list of characters,
+    discarding the punctuation along the way. Then join the
+    characters in the list back into a string.
+    '''
     line = ''.join([ch for ch in line if ch not in ex])
     for word in line.split():
         count[word] = count.get(word, 0) + 1
+
+    '''
+    count.get(word) will return count[word] if it exists, or None
+    if it doesn't. Instead we use count.get(word,0) which will
+    return 0 if count[word] doesn't exist. So the new count will
+    be the old count (or 0) plus 1.
+    '''
 
 for key in sorted(count, key=count.get):
     print(key, count[key])
